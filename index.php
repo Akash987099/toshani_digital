610a0dbb273a8d8dd23103219606be56c4bb075b
LAPTOP-O3Q8IH84\91847 <pradhuman>
2024-12-04 22:11:21 +0530

    hrthyh

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
                         d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L80 128c-8.8 0-16-7.2-16-16s7.2-16 16-16l368 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L64 32zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
                       />
                     </svg>
-                    <!-- <img src="icon/debit-card.png" alt=""> -->
+                    <!-- <i class="fa fa-code fa-7x mb-4 text-primary"></i> -->
                     <div class="flex flex-col items-start">
                       <h5 class="text-primary font-semibold text-lg">
                         Payment Services
@@ -380,7 +380,7 @@ We aggregates various financial services and provides to 500 Mn people at bottom
             <img
               class="inline"
               width="500"
-              src="images/Multifunctional-Kiosk.jpg"
+              src="inag/abour-5.jpg"
               alt="alternative"
             />
           </div>
