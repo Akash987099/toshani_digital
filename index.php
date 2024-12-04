3fb8e6119e27d777e6f1a342990dc941dd9b08ae
LAPTOP-O3Q8IH84\91847 <pradhuman>
2024-12-04 21:57:39 +0530

    gkthgthb

--- index.php
+++ index.php
@@ -63,20 +63,13 @@
         <div class="grid grid-cols-3 gap-8">
           <!-- Card -->
           <div class="flip-container border border-solid">
+
             <div class="flip-card">
               <div class="flip-card-inner">
+
                 <div class="flip-card-front px-8">
                   <div class="flex flex-col pb-10 justify-end h-full">
-                    <!-- <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="50"
-                      viewBox="0 0 512 512"
-                      fill="#e10000"
-                    >
-                      <path
-                        d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L80 128c-8.8 0-16-7.2-16-16s7.2-16 16-16l368 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L64 32zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
-                      />
-                    </svg> -->
+                    
                     <img src="icon/debit-card.png" alt="">
                     <div class="flex flex-col items-start">
                       <h5 class="text-primary font-semibold text-lg">
@@ -89,6 +82,7 @@
                     </div>
                   </div>
                 </div>
+
                 <div class="flip-card-back px-8">
                   <div class="flex flex-col pb-10 justify-end h-full">
                     <svg
@@ -106,9 +100,8 @@
                         Payment Services
                       </h5>
                       <p class="tracking-wider text-white text-sm text-left">
-                        Enable different types of financial transactions through
-                        the most appropriate solutions such as BBPS, Aadhar Pay,
-                        Mobile and DTH Recharge, Cash collection Points, etc.
+                         Easy & Innovative payment services to enable fast.
+                        Secure and seamless transactions.
                       </p>
                     </div>
                   </div>
