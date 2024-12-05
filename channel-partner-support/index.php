56f464d6f4fcf37584755c349c7a3ffc456e8799
LAPTOP-O3Q8IH84\91847 <pradhuman>
2024-12-05 07:54:40 +0530

    gttrhtrht

---
+++ channel-partner-support/index.php
@@ -0,0 +1,49 @@
+<?php
+        "../header.php";
+?>
+
+    <!-- Section 1 -->
+    <section class="footer py-0">
+      <div class="max-w-screen-xl my-20 mx-auto">
+        <div class="text-center">
+          <h4
+            class="text-primary text-xl mb-0 tracking-wider uppercase font-normal"
+          >
+            Channel Partner
+          </h4>
+          <h1 class="text-secondary font-black mb-6">
+            Channel Partner Support
+          </h1>
+          <p class="tracking-wider">
+            We extend continuous support to our partners in the areas of
+            Operations and Marketing. We have a franchisee focused team that
+            addresses the problems of our franchisee partners. We consistently
+            take significant marketing initiatives to promote the Toshani brand
+            across the country.
+          </p>
+        </div>
+        <div class="py-10">
+          <div class="max-w-screen-xl mx-auto justify-center flex items-center">
+            <div class="lg:col-span-6">
+              <div class="lg:mb-0 xl:mr-14">
+                <img
+                  class="inline"
+                  src="../images/Toshani-Support-1-1024x595.png"
+                  width="800"
+                  height="800"
+                  alt="alternative"
+                />
+              </div>
+            </div>
+          </div>
+          <!-- end of container -->
+        </div>
+        <!-- end of cards-1 -->
+      </div>
+    </section>
+    <!-- End  section 1 -->
+
+
+    <?php
+            require "../footer.php";
+    ?>
