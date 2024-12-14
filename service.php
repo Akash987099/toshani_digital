6ec7a096d79645adc7b05fb916589b6bd88edafa
LAPTOP-O3Q8IH84\91847 <pradhuman>
2024-12-14 09:24:15 +0530

    nyhy

--- service.php
+++ service.php
@@ -49,16 +49,7 @@
         <div class="flex flex-col pb-10 justify-end h-full">
           <!-- Icon -->
           <div class="card-icon text-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="50"
-              viewBox="0 0 512 512"
-              fill="#ffffff"
-            >
-              <path
-                d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L80 128c-8.8 0-16-7.2-16-16s7.2-16 16-16l368 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L64 32zM416 272a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
-              />
-            </svg>
+          <i class="fa fa-code fa-2x mb-4 text-primary"></i>
           </div>
           <!-- Content -->
           <div class="flex flex-col items-start text-content">
