68bd2495a5abbdd18e20a05b00ac9dd32f6a7152
LAPTOP-O3Q8IH84\91847 <pradhuman>
2024-12-15 12:18:38 +0530

    ft5y6

--- apply-for-aws-channel-partner/index.php
+++ apply-for-aws-channel-partner/index.php
@@ -268,7 +268,6 @@
           <!-- Icon -->
           <div class="card-icon text-center">
           <i class="fa fa-code fa-2x mb-4 text-primary"></i>
-          <i class="fa fa-building-o fa-2x mb-4 text-primary" ></i>
           </div>
           <!-- Content -->
           <div class="flex flex-col items-start text-content">
@@ -287,7 +286,7 @@
         <div class="flex flex-col pb-10 justify-end h-full">
           <!-- Icon -->
           <div class="card-icon text-center">
-          <i class="fa fa-building-o fa-2x mb-4 text-primary" ></i>
+          <i class="fa fa-building-o fa-2x mb-4 text-primary"></i>
           </div>
           <!-- Content -->
           <div class="flex flex-col items-start text-content">
